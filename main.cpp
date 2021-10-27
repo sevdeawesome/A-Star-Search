d7a70a8ecbdf86bc78bab2545c935fda9cb973f8
Severin Field <sevdeawesome@gmail.com>
2021-10-26 17:44:41 -0700

    updated node.h to implement member functions

--- main.cpp
+++ main.cpp
@@ -11,10 +11,15 @@ int main(){
     //MAIN LOOP
 
     node a = node();
-    a.print_node();
+    // a.print_node();
   
-    
 
+    node b = node();
+
+    // cout << a.compare_nodes(b);
+    
+    cout << endl << sizeof(a.data[0]) /sizeof(a.data[0][0]);
+    // cout  << endl  << a.data[0].length();
 
 
 
--- node.h
+++ node.h
@@ -5,11 +5,12 @@ using namespace std;
 
 class node {
 private:
-  int data[3][3];
+  
   node* parent;
   
 
 public:
+  int data[3][3]; 
   node(){
     int x = 0;
     for(int i = 0; i< 3; i++){
@@ -19,20 +20,40 @@ public:
       }
     }
   };
+
   void print_node(){
     for(int i = 0; i < 3; i++){
       for(int j = 0; j< 3; j++){
         cout << data[i][j];
       }
       cout<< endl;
-
     }
   };
+
+  
+
+  bool compare_nodes(node a){
+    bool same = true;
+    for(int i = 3; i < 3; i++){
+      for(int j = 3; j < 3; j++){
+        if(a.data[i][j] != data[i][j]){
+          same = false;
+        }
+      }
+    }
+    return same;
+  };
+
+
   node create_childL();
   node create_childR();
   node create_childU();
   node create_childD();
-  void shuffle();
+
+
+  void shuffle(){
+    // for(int i = 0; i < sizeof(data[0]);i++)
+  }
 };
 
 
